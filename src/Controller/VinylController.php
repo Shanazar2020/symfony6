4cf8ba64aef0cc7662d021fca1eb4433525e4232
Shanazar <snizamaddinow@gmail.com>
2023-10-26 22:53:44 +0300

    feature: add some styles

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -14,8 +14,40 @@ class VinylController extends AbstractController
     public function homepage(): Response
     {
         $tracks = [
-            ['song' => 'My Way', 'artist' => 'Calwin Clays']
+            [
+                'song' => '"Gangsta\'s Paradise"',
+                'artist' => '"Coolio"',
+            ],
+            [
+                'song' => '"Waterfalls"',
+                'artist' => '"TLC"',
+            ],
+            [
+                'song' => '"Creep"',
+                'artist' => '"TLC"',
+            ],
+            [
+                'song' => '"Kiss from a Rose"',
+                'artist' => '"Seal"',
+            ],
+            [
+                'song' => '"On Bended Knee"',
+                'artist' => '"Boyz II Men"',
+            ],
+            [
+                'song' => '"Another Night"',
+                'artist' => '"Real McCoy"',
+            ],
+            [
+                'song' => '"Fantasy"',
+                'artist' => '"Mariah Carey"',
+            ],
+            [
+                'song' => '"Take a Bow"',
+                'artist' => '"Madonna"',
+            ],
         ];
+
         return $this->render('vinyl/homepage.html.twig', [
             'title' => 'PB & Jams',
             'tracks' => $tracks
@@ -25,11 +57,9 @@ class VinylController extends AbstractController
     #[Route('/browse/{slug}')]
     public function browse(string $slug = null): Response
     {
-        if ($slug){
-            $title = 'Genre: ' . u(str_replace('-', ' ', $slug))->title(true);
-        } else {
-            $title = 'All genres';
-        }
-        return new Response($title);
+        $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
+        return $this->render('vinyl/browse.html.twig',[
+            'genre' => $genre,
+        ]);
     }
 }
