e901b2df67ecfd3ee97f8da719fd0bbc75630cd9
Shanazar <snizamaddinow@gmail.com>
2023-10-26 23:17:28 +0300

    feature: adding logger to controller

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,13 +10,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class SongController extends AbstractController
 {
     #[Route('/api/songs/{id<\d+>}', name: 'app_song_getsong', methods: ["GET"])]
-    public function getSong(int $id): Response
+    public function getSong(int $id, LoggerInterface $logger): Response
     {
         $song = [
             'id' => $id,
             'name' => 'Skyfall',
             'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
         ];
+
+        $logger->info('Requested song {song}', [
+            'song' => $id
+        ]);
 
         return $this->json($song);
     }
