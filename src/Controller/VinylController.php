6bdf31c29b99e075bb7375c863543b514853f1f6
Shanazar <snizamaddinow@gmail.com>
2023-10-27 23:32:51 +0300

    feature: add more mixes and knp time bundle.

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -14,43 +14,17 @@ class VinylController extends AbstractController
     public function homepage(): Response
     {
         $tracks = [
-            [
-                'song' => '"Gangsta\'s Paradise"',
-                'artist' => '"Coolio"',
-            ],
-            [
-                'song' => '"Waterfalls"',
-                'artist' => '"TLC"',
-            ],
-            [
-                'song' => '"Creep"',
-                'artist' => '"TLC"',
-            ],
-            [
-                'song' => '"Kiss from a Rose"',
-                'artist' => '"Seal"',
-            ],
-            [
-                'song' => '"On Bended Knee"',
-                'artist' => '"Boyz II Men"',
-            ],
-            [
-                'song' => '"Another Night"',
-                'artist' => '"Real McCoy"',
-            ],
-            [
-                'song' => '"Fantasy"',
-                'artist' => '"Mariah Carey"',
-            ],
-            [
-                'song' => '"Take a Bow"',
-                'artist' => '"Madonna"',
-            ],
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
         ];
 
         return $this->render('vinyl/homepage.html.twig', [
             'title' => 'PB & Jams',
-            'tracks' => $tracks
+            'tracks' => $tracks,
         ]);
     }
 
@@ -58,8 +32,36 @@ class VinylController extends AbstractController
     public function browse(string $slug = null): Response
     {
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
-        return $this->render('vinyl/browse.html.twig',[
+        $mixes = $this->getMixes();
+
+        return $this->render('vinyl/browse.html.twig', [
             'genre' => $genre,
+            'mixes' => $mixes,
         ]);
+    }
+
+    private function getMixes(): array
+    {
+        // temporary fake "mixes" data
+        return [
+            [
+                'title' => 'PB & Jams',
+                'trackCount' => 14,
+                'genre' => 'Rock',
+                'createdAt' => new \DateTime('2021-10-02'),
+            ],
+            [
+                'title' => 'Put a Hex on your Ex',
+                'trackCount' => 8,
+                'genre' => 'Heavy Metal',
+                'createdAt' => new \DateTime('2022-04-28'),
+            ],
+            [
+                'title' => 'Spice Grills - Summer Tunes',
+                'trackCount' => 10,
+                'genre' => 'Pop',
+                'createdAt' => new \DateTime('2019-06-20'),
+            ],
+        ];
     }
 }
